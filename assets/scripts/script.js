4f23f0d4cea03da37fec52cc92a6ec6f9069ca6c
DanWilliams91 <danielwillliams1991@gmail.com>
2022-02-24 21:14:14 +0000

    textAreaRevision function added and testing successful. gridDisplayUpdate function added but currently a work in progress.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -8,10 +8,13 @@ $(document).ready(function(){
     game {
       stage - integer (to track which stage the player is on)
     }
-  landingPageAnimation() - animates through the squares until the game is started
+      landingPageAnimation() - animates through the squares until the game is started
   startGame() - function (event listener on Play Game text) which will start a new game
+  gridDisplayUpdate() - function which adds new squares to the grid according to the stage and updates their classes
   stageBegin() - shows a countdown before the stage starts, then calls stageInPlay
+  stageInfo() - updates the timer and stage number below the game area
   stageInPlay() - squares are highlighted and timer countdown begins
+  timerStart() - In-play countdown timer begins
   stageEnd() - called when the timer reaches 0. Squares are re-hidden and the player
                 is asked to enter their answer.
   checkAnswer() - compares the player's input to the correct answer
@@ -22,7 +25,9 @@ let game = {
   stage: 0
 }
 
-var textArea = $("#bottom-text-container");
+let textArea = $("#bottom-text-container");
+let gridArea = $("#squares-container");
+let singleSquareHTML = `<div class="square"></div>`;
 
 /**
  * Iterates through the existing squares on the page and highlights random
@@ -30,9 +35,9 @@ var textArea = $("#bottom-text-container");
  */
 function initialAnimation() {
 
+  //Array shuffling function method taken from https://www.geeksforgeeks.org/how-to-shuffle-an-array-using-javascript/
   function shuffleArray(array) {
     for (var i = array.length - 1; i > 0; i--) {
-      // Generate random number
       var j = Math.floor(Math.random() * (i + 1));
       var temp = array[i];
       array[i] = array[j];
@@ -52,32 +57,92 @@ function initialAnimation() {
       $(squaresArray[7]).removeClass("red-square");
       $(squaresArray[15]).removeClass("red-square");
     }, 800);
-    $("#start").click(function(event) {
+    $("#start").on("click", function() {
+      console.log("red squares cleared");
+      $(squaresArray[0]).removeClass("red-square");
+      $(squaresArray[7]).removeClass("red-square");
+      $(squaresArray[15]).removeClass("red-square");
       clearInterval(animation);
     });
   }, 800);
 };
 
 function startGame() {
-  game.stage++;
-  if (game.stage > 2) {
-    //add squares
-  } else if (game.stage > 4) {
-    //add squares
-  } else if (game.stage > 7) {
-    //add squares
-  }
-  textArea.empty();
+  console.log("startGame() running - player clicked 'Play Game'");
+  game.stage = 1;
+  textAreaRevision();
+  gridDisplayUpdate();
 }
 
-$("#start").click(function() {
+
+/**
+ * Changes the IDs of the paragraphs in the textArea and changes their inner HTML
+ * to the text viewed when the game is in play.
+ * INCOMPLETE
+ */
+function textAreaRevision() {
+  console.log("textAreaRevision() - text below grid revised");
+  if (game.stage == 1) {
+    $("#start").attr("id", "timer");
+    $("#timer").html(`Time Remaining: 5.00`) // TIMER AMOUNT NEEDS TO BE INSERTED HERE
+    $("#how-to").attr("id", "stage-number");
+    $("#stage-number").html(`Stage ${game.stage}`)
+  }
+}
+
+/**
+ * Checks the current game stage and adds squares to the grid according to the stage
+ * INCOMPLETE - NEED TO CHECK CORRECT AMOUNT ADDED
+ * MAY ALSO NEED CHANGING TO INCLUDE AN OPTION FOR A NEW GAME TO START:
+ *  GRID WILL NEED TO RETURN TO NORMAL
+ */
+function gridDisplayUpdate() {
+  console.log("gridDisplayUpdate() - stage checked and squares added if needed");
+  game.stage = 3; //REMOVE AFTER TESTING
+  switch(game.stage) { //INSERTED BY GERRY - USE THIS INSTEAD OF IF STATEMENTS WHEN THERE'S MORE THAN 3
+    case 3:
+      console.log('Switch Statement');
+      break;
+    default:
+      console.log('default');
+  }
+  
+  if (game.stage > 2 && game.stage <= 4) {
+    for (let i = 0; i < 20; i++) {
+      console.log(i);
+      gridArea.append(singleSquareHTML);
+    };    
+    gridArea.children().removeClass("square-grid-4x4").addClass("square-grid-6x6");
+  } else if (game.stage > 4 && game.stage <= 7) {
+    for (let i = 0; i < 28; i++) {
+      gridArea.append(singleSquareHTML);
+    };    
+    gridArea.children().removeClass("square-grid-6x6").addClass("square-grid-8x8")
+  } else if (game.stage > 7 && game.stage <= 9) {
+    for (let i = 0; i < 36; i++) {
+      gridArea.append(singleSquareHTML);
+    };    
+    gridArea.children().removeClass("square-grid-8x8").addClass("square-grid-10x10");
+  } else if (game.stage > 9) {
+    for (let i = 0; i < 44; i++) {
+      gridArea.append(singleSquareHTML);
+    };
+    gridArea.children().removeClass("square-grid-10x10").addClass("square-grid-12x12");
+  }
+}
+
+
+
+
+
+$("#start").on("click", function() {
   startGame();
 });
 
-$("#how-to").click(function() {
+$("#how-to").on("click", function() {
   
 });
 
 
 
-module.exports = { game, initialAnimation }; //variables/functions to go here to export to test file, separated by commas
+module.exports = { game, initialAnimation, startGame, textAreaRevision, gridDisplayUpdate }; //variables/functions to go here to export to test file, separated by commas
--- assets/scripts/tests/test.js
+++ assets/scripts/tests/test.js
@@ -3,10 +3,10 @@
  */
 
 global.window = window
-global.$ = require('jquery');
+global.$ = require("jquery");
 
 const { hostname } = require("os");
-const { game, initialAnimation } = require("../script"); //imports the things from the JS file
+const { game, initialAnimation, startGame, textAreaRevision } = require("../script"); //imports the things from the JS file
 
 beforeAll(() => {
     let fs = require("fs");
@@ -17,15 +17,15 @@ beforeAll(() => {
 });
 
 describe("JS files linking correctly", () => {
-  test("test", () => {
+  test("game contains stage key", () => {
     expect("stage" in game).toBe(true);
   });
 });
 
 describe("Animation loading correctly on page load", () => {
-  test("test", () => {
-    document.addEventListener('DOMContentLoaded', function () {
-      var allSquares = document.getElementsByClassName("square");
+  test("some squares are being highlighted in red", () => {
+    document.addEventListener("DOMContentLoaded", function () {
+      let allSquares = document.getElementsByClassName("square");
       let squaresClassList = [];
       Array.from(allSquares).forEach(function (key) {
         squaresClassList.push(key.classList);
@@ -34,3 +34,29 @@ describe("Animation loading correctly on page load", () => {
     });
   });
 });
+
+describe("Clicking 'Play Game' stops the initial animation and alters the text below the game area", () => {
+  test("random squares are no longer being highlighted in red", () => {
+    document.getElementById("start").click();
+    let allSquares = document.getElementsByClassName("square");
+    let squaresClassList = [];
+    Array.from(allSquares).forEach(function (key) {
+      squaresClassList.push(key.classList);
+    })
+    expect(JSON.stringify(squaresClassList)).not.toContain("red-square");
+  });
+
+  test("the text below the game area has been changed once 'Play Game' is clicked", () => {
+    document.getElementById("start").addEventListener("click", function () {
+      let lowerParas = document.getElementById("bottom-text-container").children;
+      expect(lowerParas[0].innerHTML).not.toContain("Play Game");
+      expect(lowerParas[0].innerHTML).toContain("Time Remaining");
+      expect(lowerParas[1].innerHTML).not.toContain("How to Play");
+      expect(lowerParas[1].innerHTML).toContain("Stage");
+    });
+  });
+
+
+
+  
+})
