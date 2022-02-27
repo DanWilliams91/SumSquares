100de7730c03c342951791f884e2f55d7044453a
DanWilliams91 <danielwillliams1991@gmail.com>
2022-02-27 12:39:29 +0000

    JS code entered to: set the amount of red squares for stages 1 and 2 (other stages to follow); correctly decrease and display tdecrease and display the in-stage timer until it reaches zero; append an area for the player to enter their answer. New functions added to enable DOM manipulation based on player'stion based on player's answer.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
   stageBegin() - shows a countdown before the stage starts, then calls stageInPlay
   stageInfo() - updates the timer and stage number below the game area
   stageInPlay() - squares are highlighted and timer countdown begins
-  timerStart() - In-play countdown timer begins
+  stageTimer() - In-play countdown timer begins
   stageEnd() - called when the timer reaches 0. Squares are re-hidden and the player
                 is asked to enter their answer.
   checkAnswer() - compares the player's input to the correct answer
@@ -23,53 +23,51 @@ $(document).ready(function(){
 
 let game = {
   stage: 0,
-  timer: 5000,
+  timer: {sec: 5, mSec: "000"},
   clock: 3,
   redSquares: 0
 }
 
 let textArea = $("#bottom-text-container");
 let gridArea = $("#squares-container");
+var squaresArray = $(".square");
 let singleSquareHTML = `<div class="square"></div>`;
 
 $("#timer").hide();
 $("#stage-number").hide();
+
+//Array shuffling function method taken from https://www.geeksforgeeks.org/how-to-shuffle-an-array-using-javascript/
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
 
 /**
  * Iterates through the existing squares on the page and highlights random
  * ones in red until the game starts.
  */
 function initialAnimation() {
-
-  //Array shuffling function method taken from https://www.geeksforgeeks.org/how-to-shuffle-an-array-using-javascript/
-  function shuffleArray(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-      var j = Math.floor(Math.random() * (i + 1));
-      var temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
-    }
-    return array;
-  }
-
   let animation = setInterval(function () {
     console.log("initial animation activated")
-    let squaresArray = $(".square");
     shuffleArray(squaresArray);
-    $(squaresArray[0]).addClass("red-square");
-    $(squaresArray[7]).addClass("red-square");
-    $(squaresArray[15]).addClass("red-square");
-    setTimeout(() => {
-      $(squaresArray[0]).removeClass("red-square");
-      $(squaresArray[7]).removeClass("red-square");
-      $(squaresArray[15]).removeClass("red-square");
-    }, 400)
+    for(let i = 0; i < 4; i++) {
+      $(squaresArray[i]).addClass("red-square");
+    }
     if (game.stage !== 0) {
       console.log("red squares cleared");
       $(squaresArray).removeClass("red-square");
       clearInterval(animation);
     }
-  }, 400)
+    setTimeout(() =>{
+      $(squaresArray).removeClass("red-square");
+      console.log("squares deleted");
+    }, 400)
+  }, 800)
 }
 
 /**
@@ -86,18 +84,22 @@ function startGame() {
 
 function nextStage() {
   game.stage++;
-  gridDisplayUpdate();
-  stageBegin();  
+  game.clock = 3;  
+  $("#stage-number").html(`&nbsp;Stage ${game.stage}&nbsp;`); 
+  if (game.stage < 13) {
+    gridDisplayUpdate();
+    stageBegin();  
+  } else {
+    console.log("Game completed")
+    //Code when game is completed, i.e. player completed stage 12
+      //Final animation - flashing squares to congratulate
+      //Provide options to return to start page or start a new game
+  }
 }
-
-/**
- * Begins the countdown of the in-play time limit.
- * Timer code taken from https://stackoverflow.com/a/31106229
- */
-function timerStart() {}
 
 function stageBegin() {
   console.log(`Stage ${game.stage} Started`);
+  $("#timer").html(`&nbsp;Time Remaining: ${game.timer.sec}.${game.timer.mSec}&nbsp;`);
   gridArea.children().addClass("gray-square");
   $(gridArea).append(`
     <div id="player-start-input">
@@ -137,14 +139,20 @@ function stageInPlay() {
   console.log("Game in play");
   $("#player-start-input").remove();
   gridArea.children().removeClass("gray-square");
-  setSquares();// code to highlight certain number of squares - new f probably best
+  setStageSquares();
 }
 
-function setSquares() {
+function setStageSquares() {
+  var stageRedSquares = [3, 4, 5];
+  //Need to think of a way to have an array between the min and max allowed per stage
+  //So I can generate a random number between min and max for the stage answer
   if (game.stage < 13) {
     switch (game.stage) {
       case 1:
       case 2:
+        shuffleArray(stageRedSquares);
+        game.redSquares = stageRedSquares[0];
+        console.log("Stage squares calculated");
         //set number of red squares
       case 3:
       case 4:
@@ -162,25 +170,72 @@ function setSquares() {
       case 12:
         //set number of red and green squares
     }
-    timerStart(); // to start the timer.
+    stageApplyRedSquares();
+    stageTimer(); // to start the timer.
   } else {
     //Code when game is completed, i.e. player completed stage 12
   }
 }
 
 /**
- * Decreases the timer value in the game for display below the grid.
+ * Applies the number of red squares to the grid relevant to the
+ * game.redSquares value.
  */
-function timerStart() {
-  //Decrease game.timer in real-time
-  //when game.timer is 0, call stageEnd()
+function stageApplyRedSquares() {  
+  squaresArray = $(".square");
+  shuffleArray(squaresArray);
+  for (let i = 0; i < game.redSquares; i++) {
+    $(squaresArray[i]).addClass("red-square");
+  }
+}
+
+/**
+ * Begins the countdown of the in-play time limit.
+ */
+function stageTimer() {
+
+  let decrease = setInterval (function () {
+
+    if (parseInt(game.timer.mSec) > 0) {
+      game.timer.mSec -= 5;
+      $("#timer").html(`&nbsp;Time Remaining: ${game.timer.sec}.${parseInt(game.timer.mSec).toString().padStart(3, "0")}&nbsp;`);
+      
+    } else {
+      game.timer.mSec = 995;
+      game.timer.sec -= 1;
+      $("#timer").html(`&nbsp;Time Remaining: ${game.timer.sec}.${parseInt(game.timer.mSec).toString().padStart(3, "0")}&nbsp;`);
+    }
+
+    if (game.timer.sec == 0 && parseInt(game.timer.mSec) == 0) {
+      clearInterval(decrease);
+      $("#timer").html(`Time's Up!`);
+      stageEnd();
+    }
+
+  }, 5)
 }
 
 /**
  * Displays an area for the player to input their answer for each stage.
  */
 function stageEnd() {
-  //insert input for player
+  console.log("stage ended");
+  game.timer = {sec: 5, mSec: "000"};
+  $(".square").removeClass("red-square").addClass("gray-square");
+  
+  $(gridArea).append(`
+    <div id="player-start-input">
+      <p>How many RED squares did you count?
+        <input id="player-answer" type="number">
+        <button id="player-submit" data-type="submit">Check</button>
+      </p>
+    </div>`);
+  $("#player-answer").focus();
+  $("#player-answer").on("keydown", function(event) {
+    if (event.key === "Enter") {
+        checkAnswer();
+    }
+});
 }
 
 /**
@@ -188,10 +243,29 @@ function stageEnd() {
  * the redSquares key of game.
  */
 function checkAnswer() {
+  console.log("Checking answer...")
+  let playerAnswer = parseInt(document.getElementById("player-answer").value);
+  if (playerAnswer === game.redSquares) {
+    playerCorrect()
+  } else {
+    playerIncorrect()
+  }
+} 
+
+function playerCorrect() {
+  console.log("Player answered correctly!");
+  squaresArray.removeClass("gray-square red-square").addClass("green-square");
+}
+
+function playerIncorrect() {
+  console.log("Player answered incorrectly!");
+  squaresArray.removeClass("gray-square").addClass("red-square");
+}
+
   //compare answer to game.redSquares
   //if correct, display "you got it correct" and insert button to call stageBegin() to proceed
     // else display "you got it wrong" and insert button to call startGame() to restart
-}
+
 
 /**
  * Changes the IDs of the paragraphs in the textArea and changes their inner HTML
@@ -205,7 +279,7 @@ function textAreaRevision() {
     $("#start").hide();
     $("#how-to").hide();
     $("#timer").show();
-    $("#timer").html(`&nbsp;Time Remaining: ${game.timer}&nbsp;`); // TIMER AMOUNT NEEDS TO BE INSERTED HERE
+    $("#timer").html(`&nbsp;Time Remaining: ${game.timer.sec}.${game.timer.mSec}&nbsp;`); // TIMER AMOUNT NEEDS TO BE INSERTED HERE
     $("#stage-number").show();
     $("#stage-number").html(`&nbsp;Stage ${game.stage}&nbsp;`);   
     console.log("textAreaRevision() - text below grid revised");
